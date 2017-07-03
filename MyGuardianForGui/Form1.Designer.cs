c357d14c446c61abe9494c70f391a4d5c8f4a438
cuiguanghao崔光浩 <cuiguanghao@work.com>
2017-07-03 15:22:51 +0800

    commit

---
+++ MyGuardianForGui/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+using System.Windows.Forms;
+
+namespace MyGuardianForGui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        class ListViewNF : System.Windows.Forms.ListView
+        {
+            public ListViewNF()
+            {
+                // 开启双缓冲
+                this.SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+
+                // Enable the OnNotifyMessage event so we get a chance to filter out 
+                // Windows messages before they get to the form's WndProc
+                this.SetStyle(ControlStyles.EnableNotifyMessage, true);
+            }
+
+            protected override void OnNotifyMessage(Message m)
+            {
+                //Filter out the WM_ERASEBKGND message
+                if (m.Msg != 0x14)
+                {
+                    base.OnNotifyMessage(m);
+                }
+            }
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new MyGuardianForGui.Form1.ListViewNF();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackgroundImageTiled = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1110, 354);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "序列";
+            this.columnHeader1.Width = 38;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "名称";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "物理地址";
+            this.columnHeader3.Width = 250;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "当前状态";
+            this.columnHeader4.Width = 75;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "守护";
+            this.columnHeader7.Width = 47;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "重启次数";
+            this.columnHeader8.Width = 63;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "增加监控";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1037, 378);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "删除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(518, 378);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "守护状态切换";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 434);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "守护进程";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListViewNF listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private bool show_error;
+    }
+}
+
---
+++ MyGuardianForGui/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics; 
+
+namespace MyGuardianForGui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.Columns[1].Width = listView1.Width / 4;
+            listView1.Columns[2].Width = listView1.Width / 2;
+
+            timer1.Interval = 1000;
+            timer1.Start();
+
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+        }
+
+        void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult r = MessageBox.Show("确认关闭？", "警告", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (r.GetHashCode() == 2)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "可执行文件(*.exe)|*.exe";
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    if (fd.SafeFileName == listView1.Items[i].SubItems[1].Text &&
+                        fd.FileName == listView1.Items[i].SubItems[2].Text)
+                    {
+                        MessageBox.Show("已经存在！");
+                        return;
+                    }
+                }
+
+                int n = listView1.Items.Count;
+                listView1.Items.Add((n + 1).ToString());
+                listView1.Items[n].UseItemStyleForSubItems = false;
+
+                listView1.Items[n].SubItems.Add(fd.SafeFileName);
+                listView1.Items[n].SubItems.Add(fd.FileName);
+                listView1.Items[n].SubItems.Add("未知");
+                listView1.Items[n].SubItems.Add( "关");
+                listView1.Items[n].SubItems[3].BackColor = Color.LightGray;
+                listView1.Items[n].SubItems[4].BackColor = Color.OrangeRed;
+                listView1.Items[n].SubItems.Add("0");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("请选择需要删除的应用");
+                return;
+            }
+
+            if (listView1.SelectedItems.Count > 1)
+            {
+                MessageBox.Show("删除只能一个一个删除");
+                return;
+            }
+            string name = listView1.SelectedItems[0].SubItems[1].Text;
+            DialogResult r = MessageBox.Show("确认删除 名称："+name+" ？", "警告", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (r.GetHashCode() == 2)
+            {
+                return;
+            }
+            listView1.Items.Remove(listView1.SelectedItems[0]);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("请选择需要切换守护状态的应用");
+                return;
+            }
+
+            for ( int i = 0; i<listView1.SelectedItems.Count; i++)
+            {
+                if (listView1.SelectedItems[i].SubItems[4].Text == "关")
+                {
+                    listView1.SelectedItems[i].SubItems[4].Text = "开";
+                    listView1.SelectedItems[i].SubItems[4].BackColor = Color.LightGreen;
+                }
+                else
+                {
+                    listView1.SelectedItems[i].SubItems[4].Text = "关";
+                    listView1.SelectedItems[i].SubItems[4].BackColor = Color.LightGray;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                if (IsHave(listView1.Items[i].SubItems[1].Text) )
+                {
+                    if (!listView1.Items[i].SubItems[3].Text.Equals("运行中"))
+                    {
+                        listView1.Items[i].SubItems[3].BackColor = Color.LightGreen;
+                        listView1.Items[i].SubItems[3].Text = "运行中";
+                    }
+                    if (!listView1.Items[i].SubItems[5].Text.Equals("0"))
+                    {
+                        listView1.Items[i].SubItems[5].Text = "0";
+                    }
+                }
+                else
+                {
+                    if (!listView1.Items[i].SubItems[3].Text.Equals("停止"))
+                    {
+                        listView1.Items[i].SubItems[3].BackColor = Color.OrangeRed;
+                        listView1.Items[i].SubItems[3].Text = "停止";
+                    }
+
+                    if (listView1.Items[i].SubItems[4].Text.Equals("开"))
+                    {
+                        try
+                        {
+                            System.Diagnostics.Process.Start(listView1.Items[i].SubItems[2].Text);
+                            
+                        }
+                        catch (Exception a) 
+                        {
+                            listView1.Items[i].SubItems[4].Text = "关";
+                            listView1.Items[i].SubItems[4].BackColor = Color.HotPink;
+                            if (!show_error)
+                            {
+                                show_error = true;
+                                MessageBox.Show(listView1.Items[i].SubItems[1].Text + a.Message);
+                                show_error = false;
+                            }
+                        }
+                        listView1.Items[i].SubItems[5].Text = (Convert.ToInt32(listView1.Items[i].SubItems[5].Text) + 1).ToString();
+                    }
+                }
+            }
+        }
+
+        private bool IsHave(string name)
+        {
+            if (name.Length > 4)
+            {
+                name = name.Substring(0, name.Length - 4);
+            }
+            Process[] vProcesses = Process.GetProcesses();
+            foreach (Process vProcess in vProcesses)
+            {
+                if (vProcess.ProcessName.Equals(name))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
